24aafd0e1dad29397961b9e435600b1f3e956455
ayush <ayushagarwal.dev@gmail.com>
2025-10-08 15:41:30 +0530

    setup: users module

---
+++ src/users/users.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { TypedConfigService } from 'src/config/typed-config.service';
+import { AuthConfig } from 'src/config/auth.config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: TypedConfigService) => ({
+        secret: config.get<AuthConfig>('auth')?.jwt.secret,
+        signOptions: {
+          expiresIn: config.get<AuthConfig>('auth')?.jwt.expiresIn,
+        },
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class UsersModule {}
