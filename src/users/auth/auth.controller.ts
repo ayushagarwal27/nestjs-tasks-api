8aa927cac77d1cb2c2d857d5679b7d0808cedcd0
ayush <ayushagarwal.dev@gmail.com>
2025-10-11 15:21:35 +0530

    add: auth/profile route and test

--- src/users/auth/auth.controller.ts
+++ src/users/auth/auth.controller.ts
@@ -2,8 +2,12 @@ import {
   Body,
   ClassSerializerInterceptor,
   Controller,
+  Get,
+  NotFoundException,
   Post,
+  Request,
   SerializeOptions,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from '../create-user.dto';
@@ -11,12 +15,18 @@ import { AuthService } from './auth.service';
 import { User } from '../user.entity';
 import { LoginDto } from '../login.dto';
 import { LoginResponse } from '../login.response';
+import { IAuthRequest } from './auth.request';
+import { UserService } from '../user/user.service';
+import { AuthGuard } from '../auth.guard';
 
 @Controller('auth')
 @UseInterceptors(ClassSerializerInterceptor)
 @SerializeOptions({ strategy: 'excludeAll' })
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
 
   @Post('register')
   async register(@Body() createUserDto: CreateUserDto): Promise<User> {
@@ -27,5 +37,17 @@ export class AuthController {
   async login(@Body() loginDto: LoginDto): Promise<LoginResponse> {
     const accessToken = await this.authService.login(loginDto);
     return new LoginResponse({ accessToken });
+  }
+
+  @Get('profile')
+  @UseGuards(AuthGuard)
+  async profile(@Request() request: IAuthRequest): Promise<User> {
+    const user = await this.userService.findOneById(request.user.sub);
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    return user;
   }
 }
---
+++ src/users/auth/auth.request.ts
@@ -0,0 +1,3 @@
+export interface IAuthRequest {
+  user: { sub: string; name: string };
+}
